51764b7eb7d4024f730d05f9ecbc08fd2d9495c6
Omer Ozdil <omerozdil54@gmail.com>
2020-09-30 21:47:37 +0100

    Revert "updated"
    This reverts commit 5544ccce

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,6 +2,5 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("This is from Hello");
         System.out.println("I added another line");
-        //eu3 is awesome!!!!!
     }
 }
