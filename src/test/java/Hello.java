a845a5ed80c21635698ddb941918721a44749edd
Omer Ozdil <omerozdil54@gmail.com>
2020-09-30 23:45:00 +0100

    Added

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("This is from Hello");
         System.out.println("I added another line");
+        System.out.println("I made the push from the terminal");
 
     }
 }
